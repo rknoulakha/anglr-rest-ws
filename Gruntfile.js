1c8a02da6003050a7c55b11d4bd337b90d1fa05b
rohit <rknoulakha@gmail.com>
2016-10-17 16:19:23 +0530

    Try to deploy on heroku and git

--- Gruntfile.js
+++ Gruntfile.js
@@ -438,7 +438,7 @@ module.exports = function (grunt) {
     },
     pages: {
       options: {
-        remote: 'https://github.com/rknoulakha/anglr-rest-ws.git',   
+        remote: 'https://rknoulakha:rohit%40#101022@github.com/rknoulakha/anglr-rest-ws.git',   
         branch: 'deploy'
       }
     },
